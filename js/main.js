3e2519bfc270b64ee28a48c993627e7a71a1a189
jeha-dev <leejeha.dev@gmail.com>
2026-04-21 09:44:18 +0900

    js 수정

--- js/main.js
+++ js/main.js
@@ -3,73 +3,93 @@
 * main JS
 * --------------------------------
 */
-let fn = (function() {
-	"use strict";
+const fn = (function () {
+  "use strict";
 
-	return {
-		//공통
-		common : function(){
+  return {
+    // 공통
+    common: function () {
       fn.hideOtherList();
-			fn.scrollAnimation();
-		},
+      fn.scrollAnimation();
+    },
 
     // other list 페이지별로 제어
-    hideOtherList : function(){
-      let sectionWrapID = $(".contents").attr("id");
-      $(".other-project-link").find(".list-other li").each(function(){
-        if($(this).find("a").attr("id") == sectionWrapID){
-          $(this).closest("li").css("display","none");
-        }else {
-          $(this).closest("li").css("display","block");
+    hideOtherList: function () {
+      const contents = document.querySelector(".contents");
+      const sectionWrapID = contents ? contents.id : "";
+
+      const otherProjectLink = document.querySelector(".other-project-link");
+      if (!otherProjectLink) return;
+
+      const listItems = otherProjectLink.querySelectorAll(".list-other li");
+
+      listItems.forEach((li) => {
+        const anchor = li.querySelector("a");
+
+        if (anchor && anchor.id === sectionWrapID) {
+          li.style.display = "none";
+        } else {
+          li.style.display = "block";
         }
       });
     },
 
     // scroll animation
-    scrollAnimation : function(){
-
+    scrollAnimation: function () {
       aniEvent();
 
-      $(document).scroll(function(){
-        let scrTop = $(window).scrollTop();
-        let headerHt = $(".header-container").height();
-        let btnHome = $(".btn.home");
-        let clientHt = $(document).outerHeight();
-        let winHt = $(window).height();
-        let footerHt = $(".footer").outerHeight();
-        
+      document.addEventListener("scroll", function () {
+        const scrTop = window.scrollY;
+        const header = document.querySelector(".header-container");
+        const headerHt = header ? header.offsetHeight : 0;
+
+        const btnHome = document.querySelector(".btn.home");
+        const clientHt = document.documentElement.scrollHeight;
+        const winHt = window.innerHeight;
+
+        const footer = document.querySelector(".footer");
+        const footerHt = footer ? footer.offsetHeight : 0;
+
         // scroll event common
-        if( scrTop > headerHt){
+        if (scrTop > headerHt) {
           aniEvent();
-          btnHome.css("bottom","-10%");
+          if (btnHome) {
+            btnHome.style.bottom = "-10%";
+          }
         }
 
-        // btnHome remove
-        if(scrTop == 0){
-          btnHome.css("bottom","18px");
-        }else if (scrTop >= (clientHt - winHt) - (footerHt + 300) ){
-          btnHome.css("bottom","80px");
+        // btnHome 위치 제어
+        if (btnHome) {
+          if (scrTop === 0) {
+            btnHome.style.bottom = "18px";
+          } else if (scrTop >= (clientHt - winHt) - (footerHt + 300)) {
+            btnHome.style.bottom = "80px";
+          }
         }
-
       });
 
-      function aniEvent(){
-        $(".box-ani").each(function(){
-          let winHt = $(window).height();
-          let winTop =  $(window).scrollTop() + winHt;
-          let elOffTop = $(this).offset().top;
+      function aniEvent() {
+        const boxAniList = document.querySelectorAll(".box-ani");
 
-          if(winTop >= elOffTop){
-            $(this).addClass("aniload");
+        boxAniList.forEach((el) => {
+          const winHt = window.innerHeight;
+          const winTop = window.scrollY + winHt;
+          const elOffTop = el.getBoundingClientRect().top + window.scrollY;
+
+          if (winTop >= elOffTop) {
+            el.classList.add("aniload");
           }
         });
       }
     },
-	}
+  };
 })();
 
+window.addEventListener("load", function () {
+  const loading = document.getElementById("loading");
+  if (loading) {
+    loading.style.display = "none";
+  }
 
-$(window).on("load", function(){
-	fn.common();
+  fn.common();
 });
-
